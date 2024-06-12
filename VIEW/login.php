8ee5c8c95952a5c66fb9832c63ebb53bb65c1c9f
Eduardo Dias Souza Freire <eduardodiassouzafreire@gmail.com>
2024-06-11 22:27:16 -0300

    Create login.php

---
+++ VIEW/login.php
@@ -0,0 +1,30 @@
+<?php 
+    include_once 'C:\xampp\htdocs\projeto-escolar-php\DAL\conexao.php'; 
+
+    $usuario = $_POST['usuario']; 
+    $senha = $_POST['senha'];
+
+    echo "Senha: " . $senha . "  -  ".  md5($senha) . " <br/><br/>";
+
+    $sql = "Select * from usuario where usuario=?;";
+    $con = \dal\Conexao::conectar(); 
+    $query = $con->prepare($sql);
+
+    try{
+       $query->execute (array($usuario));
+       $linha = $query->fetch(\PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e) { echo "usuario inexistente"; }
+
+    \dal\Conexao::desconectar(); 
+
+
+  if (md5($senha) == $linha['senha']){
+       session_start();
+       $_SESSION['login'] = $usuario ;
+       //$_SESSION['nivel'] = $linha['nivel']; 
+       header("location:menu.php"); 
+    }
+    else header("location:index.php"); 
+
+    ?>
